e480958f5b69fa28e1badada7377e827f669cb2e
jackeline varela vasquez <jackelinevarela2@gmail.com>
2019-11-06 19:14:23 -0600

    trabajando en reestablecer contraseña

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const User = mongoose.model("usuario")
 const { validationResult } = require("express-validator");
 const passport = require("passport");
+const crypto = require("crypto");
+const enviarEmail = require("../handlers/email");
 
 exports.crearCuenta = function(req, res){
     res.render("crearCuenta", {
@@ -61,3 +63,101 @@ exports.logOut = function(req, res){
     req.flash("Sesión cerrada")
     res.redirect("/iniciarSesion");
 }
+
+//////////////////////////////////////////////////////////////////
+// Muestra el formulario de reseteo de contraseña
+exports.formularioReestablecerPassword = (req, res) => {
+    res.render("reestablecerPass", {
+      nombrePagina: "Reestablece tu contraseña",
+      tagline:
+        "Si ya tienes una cuenta en MoneyManager pero olvidaste tu contraseña, favor coloca tu correo electrónico."
+    });
+  };
+  
+  exports.enviarToken = async (req, res) => {
+    // Verificar si el correo electrónico es válido
+    const usuario = await User.findOne({ email: req.body.email });
+  
+    // Si el usuario no existe
+    if (!usuario) {
+      req.flash("error", ["El correo electrónico ingresado no existe"]);
+      return res.redirect("/reestablecerPassword");
+    }
+  
+    // El usuario existe, generar el token
+    usuario.token = crypto.randomBytes(20).toString("hex");
+    usuario.expira = Date.now() + 3600000;
+  
+    // Guardar el usuario
+    await usuario.save();
+  
+    // Generar la URL
+    const resetUrl = `http://${req.headers.host}/reestablecerPass/${usuario.token}`;
+  
+    // Enviar la notificación por email
+    await enviarEmail.enviar({
+      usuario,
+      subject: "Reestablecer tu contraseña",
+      template: "resetPassword",
+      resetUrl
+    });
+  
+    // Redireccionar
+    req.flash("correcto", [
+      "Verifica tu correo electrónico para seguir las instrucciones"
+    ]);
+    res.redirect("/iniciarSesion");
+  };
+  
+  // Mostrar el formulario de cambio de contraseña
+  exports.formularioNuevoPassword = async (req, res) => {
+    // buscar el usuario por medio del token y la fecha de expiración
+    const usuario = await Usuario.findOne({
+      token: req.params.token,
+      expira: { $gt: Date.now() }
+    });
+  
+    // No se pudo encontrar el usuario con el token o token vencido
+    if (!usuario) {
+      req.flash("error", [
+        "Solicitud expirada. Vuelve a solicitar el cambio de contraseña"
+      ]);
+      return res.redirect("/reestablecerPassword");
+    }
+  
+    // Mostrar el formulario de nueva password
+    res.render("nuevaPassword", {
+      nombrePagina: "Ingresa tu nueva contraseña",
+      tagline: "Asegurate de utilizar una contraseña segura"
+    });
+  };
+  
+  // Almacena la nueva contraseña
+  exports.almacenarNuevaPassword = async (req, res) => {
+    // buscar el usuario por medio del token y la fecha de expiración
+    const usuario = await Usuario.findOne({
+      token: req.params.token,
+      expira: { $gt: Date.now() }
+    });
+  
+    // No se pudo encontrar el usuario con el token o token vencido
+    if (!usuario) {
+      req.flash("error", [
+        "Solicitud expirada. Vuelve a solicitar el cambio de contraseña"
+      ]);
+      return res.redirect("/reestablecerPassword");
+    }
+  
+    // Obtener el nuevo password
+    usuario.password = req.body.password;
+    // Limpiar los valores que ya no son requeridos
+    usuario.token = undefined;
+    usuario.expira = undefined;
+  
+    // Almacenar los valores en la base de datos
+    await usuario.save();
+  
+    // Redireccionar
+    req.flash("correcto", ["Contraseña modificada correctamente"]);
+    res.redirect("/iniciarSesion");
+  };
